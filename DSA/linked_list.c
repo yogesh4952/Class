eeca1fb51805f61b8b82ea536796d45b3e95bab9
Yogesh Shah <yogeshshah2063@gmail.com>
2025-03-17 21:46:45 +0545

    Succefully implement linkedList

--- DSA/linked_list.c
+++ DSA/linked_list.c
@@ -5,12 +5,14 @@ int main()
 	struct node{
 		int data;
 		struct node *next;
-	}
+	};
 
 	int choice;
 
 	struct node *newNode, *head, *temp;
 	head = NULL;
+
+do{
 
 	newNode = (struct node *) malloc(sizeof(struct node));
 	printf("\nEnter data for newNode: ");
@@ -26,6 +28,13 @@ int main()
 
 	printf("\nDo you want to add mode node (yes for 0/ No for 1): ");
 	scanf("%d",&choice);
+}while(choice ==0);
+
+	temp = head;
+	while(temp != 0){
+		printf("%d\t",temp->data);
+		temp = temp -> next;
+	}
 
 	return 0;
 }
