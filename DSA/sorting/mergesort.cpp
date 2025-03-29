3ba3a95c664716b74043758b2c20ef905abaadf3
Yogesh Shah <yogeshshah2063@gmail.com>
2025-03-29 14:00:39 +0545

    Implementing merge sort

---
+++ DSA/sorting/mergesort.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    int left = low;
+    int right = mid + 1;
+    vector<int> temp;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] < arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mS(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mS(arr, low, mid);
+    mS(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+void mergesort(vector<int> &arr)
+{
+    mS(arr, 0, arr.size() - 1);
+}
+
+int main()
+{
+    vector<int> arr = {5, 4, 3, 2, 1};
+    mergesort(arr);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << "\t";
+    }
+}
