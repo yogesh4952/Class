0ace21f9f63aee51a9d2637a3002a9a055080419
Yogesh Shah <yogeshshah2063@gmail.com>
2025-03-29 11:57:11 +0545

    Implementing insertion sort algorithm

---
+++ DSA/sorting/insertionSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+
+    int arr[] = {7, 6, 5, 4, 3, 2, 1};
+    insertionSort(arr, 7);
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    return 0;
+}
