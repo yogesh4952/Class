54fd9b1ddb48aefa854f01b04025909c7a4e10d5
Yogesh Shah Thakuri <yogeshshah2063@gmail.com>
2025-04-17 08:18:10 +0545

    Queue practice

--- DSA/board/prac.c
+++ DSA/board/prac.c
@@ -1,28 +1,50 @@
+
 #include <stdio.h>
-void selectionSort(int arr[], int size)
+
+#define SIZE 100
+int rear = -1;
+int front = -1;
+
+int queue[SIZE];
+
+void enqueue(int x)
 {
-    for (int i = 0; i < size; i++)
+    if (rear == SIZE - 1)
     {
-        int min = i;
-        for (int j = i; j < size; j++)
-        {
-            if (arr[j] < arr[min])
-            {
-                min = j;
-            }
-            int temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
-        }
+        printf("Queue is full!\n");
+        return;
+    }
+    else
+    {
+        front = 0;
+        queue[++rear] = x;
     }
 }
 
+void dequeue()
+{
+    printf("%d is dequeud\n", front);
+    front++;
+}
+
+void display()
+{
+    for (int i = front; i <= rear; i++)
+    {
+        printf("%d\t", queue[i]);
+    }
+}
 int main()
 {
-    int arr[] = {5, 4, 3, 21, 2};
-    selectionSort(arr, 5);
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d\t", arr[i]);
-    }
+    enqueue(4);
+    enqueue(42);
+    enqueue(544);
+    enqueue(566);
+    enqueue(55555);
+
+    dequeue();
+
+    display();
+
+    return 0;
 }
