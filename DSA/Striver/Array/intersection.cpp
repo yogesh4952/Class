720dc30c61a80cb220f7ab8dba483c7bb82a8711
yogesh4952 <yogeshshah2063@gmail.com>
2025-05-27 18:08:07 +0545

    Implementing missingNumberProblem in Arrat DS

--- DSA/Striver/Array/intersection.cpp
+++ DSA/Striver/Array/intersection.cpp
@@ -1,34 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> Intersection(vector<int> &arr1, vector<int> &arr2)
+// vector<int> Intersection(vector<int> &arr1, vector<int> &arr2)
+// {
+//     int n1 = arr1.size();
+//     int n2 = arr2.size();
+
+//     vector<int> visitedArray(n2, 0);
+
+//     vector<int> intersectionArray;
+
+//     for (int i = 0; i < n1; i++)
+//     {
+//         for (int j = 0; j < n2; j++)
+//         {
+//             if (arr1[i] == arr2[j] && visitedArray[j] == 0)
+//             {
+//                 intersectionArray.push_back(arr1[i]);
+//                 visitedArray[j] = 1;
+//                 break;
+//             }
+
+//             if (arr2[j] > arr1[i])
+//             {
+//                 break;
+//             }
+//         }
+//     }
+
+//     return intersectionArray;
+// }
+
+//  Optimal Approach using 2 pointer concept
+
+vector<int> Intersection(vector<int> &a, vector<int> &b)
 {
-    int n1 = arr1.size();
-    int n2 = arr2.size();
+    int i = 0;
+    int j = 0;
 
-    vector<int> visitedArray(n2, 0);
-
-    vector<int> intersectionArray;
-
-    for (int i = 0; i < n1; i++)
+    vector<int> temp;
+    while (i < a.size() && j < b.size())
     {
-        for (int j = 0; j < n2; j++)
+        if (a[i] < b[j])
         {
-            if (arr1[i] == arr2[j] && visitedArray[j] == 0)
-            {
-                intersectionArray.push_back(arr1[i]);
-                visitedArray[j] = 1;
-                break;
-            }
 
-            if (arr2[j] > arr1[i])
-            {
-                break;
-            }
+            i++;
+        }
+        else if (a[i] > b[j])
+        {
+            j++;
+        }
+        else
+        {
+            temp.push_back(a[i]);
+            i++;
+            j++;
         }
     }
 
-    return intersectionArray;
+    return temp;
 }
 
 int main()
