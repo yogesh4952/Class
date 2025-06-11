54805e84f1def6d880b1ca4932d8f59ef47c8324
yogesh4952 <yogeshshah2063@gmail.com>
2025-06-11 15:56:51 +0545

    Solve majorityElement using moore's voting algorithm

--- DSA/Striver/Array/majorityElem.cpp
+++ DSA/Striver/Array/majorityElem.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 // Using two loops the T.C Will be o(n*n)
+/*
 int majorityElement(vector<int> &arr)
 {
   for (int i = 0; i < arr.size(); i++)
@@ -27,9 +28,83 @@ int majorityElement(vector<int> &arr)
   return -1;
 }
 
+*/
+
+// Solving the same problem using better approach
+
+/*
+int majorityElement(vector<int> &arr)
+{
+
+map<int, int> mpp;
+for (int i = 0; i < arr.size(); i++)
+{
+  mpp[arr[i]]++;
+}
+
+for (auto it : mpp)
+{
+  if (it.second > arr.size() / 2)
+  {
+    return it.first;
+  }
+}
+
+return -1;
+}
+*/
+
+// Using moore's voting algorithm
+int majorityElement(vector<int> &arr)
+{
+  int cnt = 0;
+  int el;
+
+  for (int i = 0; i < arr.size(); i++)
+  {
+    if (cnt == 0)
+    {
+      cnt = 1;
+      el = arr[i];
+    }
+    else if (arr[i] == el)
+    {
+      cnt++;
+    }
+    else
+    {
+      cnt--;
+    }
+  }
+
+  if (cnt == 0)
+  {
+    return -1;
+  }
+  cnt = 0;
+  for (int i = 0; i < arr.size(); i++)
+  {
+    if (arr[i] == el)
+    {
+      cnt++;
+    }
+  }
+
+  if (cnt > arr.size() / 2)
+  {
+    return el;
+  }
+  else
+  {
+    return -1;
+  }
+}
+
 int main()
 {
   vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
   int ans = majorityElement(arr);
+  map<int, int> mpp;
+
   cout << ans << endl;
 }
