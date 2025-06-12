13525c1d9b3b8f182ad3967e8c72c0625178f46c
yogesh4952 <yogeshshah2063@gmail.com>
2025-06-12 17:33:03 +0545

    Solve nextPermutation problem with optimized approach

---
+++ DSA/Striver/Array/rearrangeUnequal.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Rearrange function if negative and positive are not n/2
+vector<int> rearrange(vector<int> &arr)
+{
+  int n = arr.size();
+  vector<int> ans(n, 0);
+
+    return ans;
+}
+
+int main()
+{
+
+  vector<int> arr = {1, 2, 3, 1};
+  vector<int> ans = rearrange(arr);
+
+  for (int i = 0; i < ans.size(); i++)
+  {
+    cout << ans[i];
+  }
+}
