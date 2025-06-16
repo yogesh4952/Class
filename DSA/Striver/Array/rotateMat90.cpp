a0c2ba9b0e2cd8fb2c528d497323296f9c2b9dc3
yogesh4952 <yogeshshah2063@gmail.com>
2025-06-16 20:25:11 +0545

    rotate matrix using extra array

--- DSA/Striver/Array/rotateMat90.cpp
+++ DSA/Striver/Array/rotateMat90.cpp
@@ -1,24 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rotate(const vector<vector<int>> &arr)
+void rotate(vector<vector<int>> &arr)
 {
   int n = arr.size();
   int m = arr[0].size();
-  vector<vector<int>> ans(m, vector<int>(n));
+
+  // Here we're solving this problem using optmial approach
+
+  /*
+    1) Transpose matrix
+    2) Reverse the row
+  */
 
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < m; j++)
     {
-      ans[j][n - 1 - i] = arr[i][j];
+      arr[i][j] = arr[j][i];
     }
   }
 
-  for (auto &row : ans)
+  for (int i = 0; i < n; i++)
   {
-    for (int val : row)
-      cout << val << ' ';
+    for (int j = 0; j < m; j++)
+    {
+      cout << arr[i][j] << " ";
+    }
     cout << endl;
   }
 }
