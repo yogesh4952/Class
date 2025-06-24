a5259e1b844bca5651d15a61a362f017bb43ea26
yogesh4952 <yogeshshah2063@gmail.com>
2025-06-24 16:55:30 +0545

    rotated search array with unique element

---
+++ DSA/Striver/Binary Search/BS on 1D Array/rotatedArSearchI.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> arr, int target)
+{
+  int low = 0;
+  int high = arr.size() - 1;
+
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (arr[mid] == target)
+      return mid;
+
+    // Left sorted
+    if (arr[low] <= arr[mid])
+    {
+      if (arr[low] <= target && target <= arr[mid])
+      {
+        high = mid - 1;
+      }
+      else
+        low = mid + 1;
+    }
+
+    // Right Sorted
+
+    else
+    {
+      if (arr[mid] <= target && target <= arr[high])
+      {
+        low = mid + 1;
+      }
+      else
+        high = mid - 1;
+    }
+  }
+
+  return -1;
+}
+int main()
+{
+
+  vector<int> arr = {6, 7, 8, 9, 1, 2, 3, 4, 5};
+  cout << search(arr, 9);
+}
