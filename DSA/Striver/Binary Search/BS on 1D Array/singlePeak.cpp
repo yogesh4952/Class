2f7bca1e30a593caf53822ac3ab297c70e94ce09
yogesh4952 <yogeshshah2063@gmail.com>
2025-06-30 17:39:22 +0545

    SOlve single peak element problem

---
+++ DSA/Striver/Binary Search/BS on 1D Array/singlePeak.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findPeak(vector<int> &arr)
+{
+
+  int n = arr.size();
+
+  if (arr[0] > arr[1])
+    return arr[0];
+
+  if (arr[n - 1] > arr[n - 2])
+    return arr[n - 1];
+
+  int low = 1;
+  int high = n - 2;
+
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+      return arr[mid];
+
+    // Elimination
+
+    if (arr[mid] < arr[mid + 1])
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      high = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+int main()
+{
+
+  vector<int> arr = {1, 2, 3, 4, 5, 3, 2, 1};
+  cout << findPeak(arr) << endl;
+  return 0;
+}
